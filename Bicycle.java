4ab4c688266e3b9dc85cc3adc4762f25480ded2c
Danny Mexen <danny@arcariusmexen.com>
2022-06-25 02:24:00 +0200

    Setters and methods

--- Bicycle.java
+++ Bicycle.java
@@ -13,4 +13,22 @@ public class Bicycle {
         cadence = startCadence;
         speed = startSpeed;
     }
+
+    // the Bicycle class has
+    // four methods
+    public void setCadence(int newValue) {
+        cadence = newValue;
+    }
+
+    public void setGear(int newValue) {
+        gear = newValue;
+    }
+
+    public void applyBrake(int decrement) {
+        speed -= decrement;
+    }
+
+    public void speedUp(int increment) {
+        speed += increment;
+    }
 }
